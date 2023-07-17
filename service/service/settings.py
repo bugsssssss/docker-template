a2fa178c4558c2c67e232669c0765cb973370db2
bugsssssss <n.soatov@bk.ru>
2023-07-17 12:39:33 +0500

    db update

--- service/service/settings.py
+++ service/service/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
 }
 
 DATABASES = {
-    'defaukt': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
